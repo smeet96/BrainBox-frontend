adccc7df0bffa6da86c08bc511e9de8ac250feb1
smeet96 <smeetsherki@gmail.com>
2025-10-01 12:56:57 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='bg-amber-800 underline'>
+      Heloo ji namsate welcome to chai aur code 
+    </div>
+  )
+}
+
+export default App
