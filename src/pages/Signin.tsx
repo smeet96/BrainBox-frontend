a75d7cc7f93ce3a8c96e423d4e9ebed40874d4e5
smeet96 <smeetsherki@gmail.com>
2025-10-03 14:09:29 +0530

    added functionality

--- src/pages/Signin.tsx
+++ src/pages/Signin.tsx
@@ -1,14 +1,35 @@
+import { useState } from "react"
 import Button from "../components/Button"
 import Input from "../components/Input"
+import axios from "axios"
+import { useNavigate } from "react-router-dom"
+
+
 
 const Signin = () => {
+    const [email , setEmail] = useState("")
+    const [password , setPassword] = useState("")
+    const navigate = useNavigate()
+
     return (
         <div className="bg-violet-950 h-screen w-screen pt-66">
-        <form className="max-w-sm mx-auto">
-        <Input label="Your email" placeholder="john@gmail.com" type="email" onChange={(e)=> {console.log(e.target.value)}} /> 
-        <Input label="Password" placeholder="1234567" type="password" onChange={(e)=> {console.log(e.target.value)}}/>
-        <Button placeholder="Signin" onClick={async() => { await console.log("axios.post")}}/> 
-        </form>
+        <div className="max-w-sm mx-auto">
+        <Input label="Your email" placeholder="john@gmail.com" type="email" onChange={(e)=> {setEmail(e.target.value)}} /> 
+        <Input label="Password" placeholder="1234567" type="password" onChange={(e)=> {setPassword(e.target.value)}}/>
+        <Button placeholder="Signin" onClick={async() => {
+           try {
+            const signin = await axios.post("http://localhost:3000/api/v1/user/signin",{
+                  email,
+                  password
+                   }               
+               )
+               localStorage.setItem("token" , signin.data.token)
+           } catch (error) {
+           return console.log("internal error")
+           } 
+    navigate("/dashboard")
+        }}/> 
+        </div>
         </div>
     )
 }
